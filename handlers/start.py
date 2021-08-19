b6c524d84f76fa723b068ad0468da7bd55fcc7b0
Kennedy <abdulmutaqin14@gmail.com>
2021-08-19 13:53:50 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -87,7 +87,7 @@ async def start(client: Client, message: Message):
         )
     )
 
-@Client.on_message(command(["help", f"help@{BOT_USERNAME}]") & filters.group & ~filters.edited)
+@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_text(
         f"""<b>👋🏻 Hello {message.from_user.mention()}, please tap the button below to see the help message you can read for using this bot</b>""",

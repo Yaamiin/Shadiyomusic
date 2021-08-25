88840aa12a0c4f74bd0553a3e8310f86f105a098
Kennedy <abdulmutaqin14@gmail.com>
2021-08-25 23:16:49 +0700

    Create asupan.py

---
+++ handlers/asupan.py
@@ -0,0 +1,36 @@
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
+
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+
+@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`404 asupan videos not found:v`")
+
+
+@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`404 wibu not found:v`")
+
+
+@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
+async def chika(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`404 chika videos not found`")

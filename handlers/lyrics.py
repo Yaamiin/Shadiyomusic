34311afe425080bc05000cbd63aec6d1bc689f75
Kennedy <abdulmutaqin14@gmail.com>
2021-08-25 23:14:10 +0700

    Update lyrics.py

--- handlers/lyrics.py
+++ handlers/lyrics.py
@@ -2,8 +2,8 @@
 # Copyright (C) 2021 TdMusic
 
 import requests
-from pyrogram import Client
 from config import BOT_USERNAME
+from pyrogram import Client
 from helpers.filters import command
 
 
@@ -11,12 +11,12 @@ from helpers.filters import command
 async def lirik(_, message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("**give a lyric name too !**")
+            await message.reply_text("**Kasih judul lagunya lah blok!!**")
             return
         query = message.text.split(None, 1)[1]
-        rep = await message.reply_text("🔎 **searching lyrics...**")
-        resp = requests.get(f"https://tede-api.herokuapp.com/api/lirik?l={query}").json()
+        rep = await message.reply_text("🔁 **Tunggu bentar ya!**")
+        resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
         result = f"{resp['data']}"
         await rep.edit(result)
     except Exception:
-        await rep.edit("**Lyrics not found.** please give a valid song name !")
+        await rep.edit("**Lirik tidak ditemukan.** Coba cari dengan judul lagu yang lebih jelas")

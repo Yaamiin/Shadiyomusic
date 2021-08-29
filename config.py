9e3c6c28796e09047fd6c9cd3e55cdeabb2a01af
Kennedy <abdulmutaqin14@gmail.com>
2021-08-29 07:42:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -25,11 +25,6 @@ OWNER_NAME = getenv("OWNER_NAME", "xgothboi") # isi dengan username kamu tanpa s
 DEV_NAME = getenv("DEV_NAME", "xgothboi")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 
-OWNER_ID = int(os.environ.get("OWNER_ID")) # fill with your id as the owner of the bot
-DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # make a private channel and get the channel id
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
-
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "30"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())

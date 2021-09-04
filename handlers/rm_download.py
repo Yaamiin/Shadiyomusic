b14f82c09246848c6ac07462bb4d828246bbbf4b
Kennedy <abdulmutaqin14@gmail.com>
2021-09-04 11:31:47 +0700

    Update rm_download.py

--- handlers/rm_download.py
+++ handlers/rm_download.py
@@ -7,6 +7,7 @@ from helpers.filters import command
 from helpers.decorators import sudo_users_only, errors
 
 downloads = os.path.realpath("downloads")
+raw = os.path.realpath("raw_files")
 
 @Client.on_message(command(["rmd", "rmdownloads", "cleardownloads"]) & ~filters.edited)
 @errors
@@ -19,3 +20,15 @@ async def clear_downloads(_, message: Message):
         await message.reply_text("✅ **Menghapus semua file yang didownload**")
     else:
         await message.reply_text("❌ **File terdownload kosong, sama seperti hatimu!**")
+
+@Client.on_message(command(["clean", "wipe", "rmr"]) & ~filters.edited)
+@errors
+@sudo_users_only
+async def clear_raw(_, message: Message):
+    ls_dir = os.listdir(raw)
+    if ls_dir:
+        for file in os.listdir(raw):
+            os.remove(os.path.join(raw, file))
+        await message.reply_text("✅ **Menghapus semua file raw**")
+    else:
+        await message.reply_text("❌ **File raw kosong, sama seperti hidupmu!**")

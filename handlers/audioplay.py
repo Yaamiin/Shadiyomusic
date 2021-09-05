35b896fd899692fcbaa326f2b0b7be50d90e6bbc
Kennedy <abdulmutaqin14@gmail.com>
2021-09-05 13:15:04 +0700

    Update audioplay.py

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -25,15 +25,6 @@ from PIL import Image, ImageFont, ImageDraw
 aiohttpsession = aiohttp.ClientSession()
 
 
-def changeImageSize(maxWidth, maxHeight, image):
-    widthRatio = maxWidth / image.size[0]
-    heightRatio = maxHeight / image.size[1]
-    newWidth = int(widthRatio * image.size[0])
-    newHeight = int(heightRatio * image.size[1])
-    newImage = image.resize((newWidth, newHeight))
-    return newImage
-
-
 async def generate_cover(requested_by, title, views, duration, thumbnail):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:

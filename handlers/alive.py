cfb57432d95cf2a825e9b08889d8375484363b95
Kennedy <abdulmutaqin14@gmail.com>
2021-09-06 10:09:29 +0700

    Update alive.py

--- handlers/alive.py
+++ handlers/alive.py
@@ -1,4 +1,4 @@
-from config import BOT_USERNAME, BOT_IMAGE, OWNER_NAME, GROUP_SUPPORT, UPDATES_CHANNEL
+from config import BOT_USERNAME, BOT_NAME, OWNER_NAME, GROUP_SUPPORT, UPDATES_CHANNEL
 from helpers.filters import command
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
@@ -7,10 +7,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command("alive") & filters.group & ~filters.edited)
 async def alive(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"{BOT_IMAGE}",
+        photo="https://telegra.ph/file/fbffad50c0cff6c9001cf.jpg",
         caption=f"""**Heyyo I'm alive Sir**
 
-🔴 **[This bot](https://t.me/{BOT_USERNAME}) Is online**
+🔴 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Is online**
 🔴 **Im working properly**
 🔴 **Bot ver:** `3.0` Latest
 🔴 **Python Ver :** `3.9.7`

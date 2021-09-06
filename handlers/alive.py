2947d58dbe02ff77b8a633c18a1daeb881220725
Kennedy <abdulmutaqin14@gmail.com>
2021-09-06 11:11:28 +0700

    Update alive.py

--- handlers/alive.py
+++ handlers/alive.py
@@ -7,39 +7,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.command(["alive", f"alive@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["alive", f"alive@{BOT_USERNAME}"]) & filters.group & filters.private)
 async def alive(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"{BOT_IMG}",
-        caption=f"""**Heyyo I'm alive Sir**
-
-🔴 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Is online**
-🔴 **Im working properly**
-🔴 **Bot ver :** `5.0` Latest
-🔴 **Python Ver :** `3.9.7`
-🔴 **My Master : [{OWNER_NAME}](https://t.me/{OWNER_NAME})**
-
-**Thanks For Using me 🕊️**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "👥 Group", url=f"https://t.me/{GROUP_SUPPORT}"
-                    ),
-                    InlineKeyboardButton(
-                        "📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
-                ],[
-                    InlineKeyboardButton(
-                        "⚙️ Source Code ⚙️", url="https://github.com/KennedyProject/KennedyXMusic"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_message(filters.command("alive") & filters.private & ~filters.edited)
-async def alive_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{BOT_IMG}",
         caption=f"""**Heyyo I'm alive Sir**

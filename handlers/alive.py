d2383815701b53c1d09516da6aef663949d2cd63
Kennedy <abdulmutaqin14@gmail.com>
2021-09-06 12:21:05 +0700

    Create alive.py

---
+++ handlers/alive.py
@@ -0,0 +1,33 @@
+from config import BOT_IMG, BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, OWNER_NAME, UPDATES_CHANNEL, GROUP_SUPPORT
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["alive", f"alive@{BOT_USERNAME}"]))
+async def alive(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{BOT_IMG}",
+        caption=f"""**Heyyo I'm alive Sir**
+🔴 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Is online**
+🔴 **Im working properly**
+🔴 **Bot ver :** `5.0` Latest
+🔴 **Python Ver :** `3.9.7`
+🔴 **My Master : [{OWNER_NAME}](https://t.me/{OWNER_NAME})**
+**Thanks For Using me 🕊️**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "👥 Group", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
+                ],[
+                    InlineKeyboardButton(
+                        "⚙️ Source Code ⚙️", url="https://github.com/KennedyProject/KennedyXMusic"
+                    )
+                ]
+            ]
+        )
+    )

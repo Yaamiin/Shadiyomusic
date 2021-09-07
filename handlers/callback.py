5e76c487ea2ee0bd6b26d6c155969e03cf83c141
Kennedy <abdulmutaqin14@gmail.com>
2021-09-07 14:29:03 +0700

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -1,10 +1,62 @@
 # (C) supun-maduraga my best friend for his project on call-music-plus
 
+from time import time
+from datetime import datetime
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
 from helpers.decorators import authorized_users_only
 from config import BOT_NAME, BOT_USERNAME, OWNER_NAME, GROUP_SUPPORT, UPDATES_CHANNEL, ASSISTANT_NAME
 from handlers.play import cb_admin_check
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_callback_query(filters.regex("cbstart")& filters.group & ~filters.edited)
+async def cbstart(_, query: CallbackQuery):
+    uptime = await _human_time_duration(int(uptime_sec))
+    await query.edit_message_text(
+        f"""<b>👋🏻 **Hello {message.from_user.mention()}!**</b>
+
+✅ **Saya aktif dan siap memutar musik!**
+• **Uptime :** `{uptime}`
+• Klik pada tombol » 📚 **Perintah** dan lihat semua perintah bot!
+
+💡 Bot By @{UPDATES_CHANNEL}**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "👥 Support", url=f"https://t.me/{GROUP_SUPPORT}")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📚 Perintah", callback_data="cbhelp"
+                    )
+                ]
+            ]
+        )
+    )
+
 
 
 @Client.on_callback_query(filters.regex("cbhelp"))

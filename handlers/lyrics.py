d70159dd160f1bbc8ba57642017bcaf15464a05c
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2021-09-17 01:24:13 +0700

    y

--- handlers/lyrics.py
+++ handlers/lyrics.py
@@ -11,12 +11,12 @@ from helpers.filters import command
 async def lirik(_, message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("**Kasih judul lagunya lah blok!!**")
+            await message.reply_text("`Kasih judul lagunya lah blok!!!`")
             return
         query = message.text.split(None, 1)[1]
-        rep = await message.reply_text("**Bentar...**")
+        rep = await message.reply_text("`Searching lyrics...`")
         resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
         result = f"{resp['data']}"
         await rep.edit(result)
     except Exception:
-        await rep.edit("**Lirik tidak ditemukan.** Coba cari dengan judul lagu yang lebih jelas")
+        await rep.edit("`Lirik tidak ditemukan !`\n\n• `Coba cari dengan judul lagu yang lebih jelas`")

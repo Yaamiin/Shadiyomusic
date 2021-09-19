c0bb0ff4b937179dce2d4131bfc75970f95afa9e
Kennedy <87056837+KennedyProject@users.noreply.github.com>
2021-09-19 09:29:40 +0700

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -65,20 +65,22 @@ async def cbabout(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>❓ **About  [{bn}](https://t.me/{BOT_USERNAME})**</b> 
 
-➠ **A powerfull bot for playing music in your groups!
+➠ **A powerfull bot for management and playing music in your groups!
 
 ➠ Working with pyrogram
 
-➠ Using Python 3.9.7
+➠ Python 3.9.7
 
-➠ I can playing music and download videos from YouTube
+➠ Telethon 1.23.0
 
-➠ I can make you happy :v
+➠ Download videos and music from YouTube
+
+➠ Amd more extra features
 
 __{bn} licensed under the GNU General Public License v.3.0__
 
-• Updates channel @{UPDATES_CHANNEL}
-• Group Support @{GROUP_SUPPORT}
+• Updates [channel](https://t.me/{UPDATES_CHANNEL})
+• Group [Support](https://t.me/{GROUP_SUPPORT})
 • Assistant @{ASSISTANT_NAME}
 • Here is my [Owner](https://t.me/{OWNER_NAME})**""",
         reply_markup=InlineKeyboardMarkup(
@@ -94,76 +96,6 @@ __{bn} licensed under the GNU General Public License v.3.0__
             ]
         ),
      disable_web_page_preview=True
-    )
-
-
-@Client.on_callback_query(filters.regex("cbhelp"))
-async def cbhelp(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""<b>🕊️ Here is the help menu !</b>
-
-**In this menu you can open several available command menus, in each command menu there is also a brief explanation of each command**
-
-💡 Bot by @{UPDATES_CHANNEL}""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "📚 Basic Cmd", callback_data="cbbasic"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "📘 Admin Cmd", callback_data="cbadmin"
-                    ),
-                    InlineKeyboardButton(
-                        "📗 Sudo Cmd", callback_data="cbsudo"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "📔 Fun Cmd", callback_data="cbfun"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbguide"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("cbbasic"))
-async def cbbasic(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""<b>🕊️ basic commands for bots
-
-[GROUP SETTINGS]
-/play (title) - play music via youtube
-/ytp (title) - play music live
-/stream (reply to audio) - play music via reply to audio
-/playlist - view queue list
-/song (title) - download music from youtube
-/search (title) - search for music from youtube in detail
-/video (title) - download music from youtube in detail
-/lyrics - (title) search for lyrics
-[ MORE ]
-/alive - check alive bot
-/start - starting bot
-/ping - show current ping
-
-💡 Bot by @{UPDATES_CHANNEL}""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbhelp"
-                    )
-                ]
-            ]
-        )
     )
 
 
@@ -192,173 +124,6 @@ async def cbadvanced(_, query: CallbackQuery):
                     ),
                     InlineKeyboardButton(
                         "ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("cbadmin"))
-async def cbadmin(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""<b>🕊️ command for group admin
-
-/player - view playback status
-/pause - pauses playing music
-/resume - resume paused music
-/skip - skip to next song
-/end - mute the music
-/userbotjoin - invite assistant to join the group
-/musicplayer (on / off) - turn on / off the music player in your group
-
-💡 Bot by @{UPDATES_CHANNEL}""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbhelp"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("cbsudo"))
-async def cbsudo(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""<b>🕊️ **command for sudo**
-
-**/userbotleaveall - remove assistant from all groups
-/gcast - send global messages via assistant
-/rmd - delete downloaded files
-/rmr - deletes downloaded raw files
-
-💡 Bot by @{UPDATES_CHANNEL}**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbhelp"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("cbfun"))
-async def cbfun(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""<b>🕊️ **Command fun**
-
-**/chika - check it yourself
-/wibu - check it yourself
-/asupan - check yourself
-/truth - check yourself
-/dare - check it yourself
-
-💡 Bot by @{UPDATES_CHANNEL}**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbhelp"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("cbguide"))
-async def cbguide(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""**HOW TO USE THIS BOTT :**
-
-**1.) First, add to your group.
-2.) Then make admin with all permissions except anonymous admin.
-3.) Add @{ASSISTANT_NAME} to your group or type `/userbotjoin` to invite assistant.
-4.) Turn on voice chat first before playing music.
-
-💡 Bot by @{UPDATES_CHANNEL}**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "📚 Command List", callback_data="cbhelp"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🗑 Close", callback_data="close"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("close"))
-async def close(_, query: CallbackQuery):
-    await query.message.delete()
-
-
-@Client.on_callback_query(filters.regex("cbcmds"))
-async def cbhelps(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""<b>🕊️ **Here is the help menu !</b>
-
-**In this menu you can open several available command menus, in each command menu there is also a brief explanation of each command**
-
-💡 Bot by @{UPDATES_CHANNEL}**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "📚 Basic Cmd", callback_data="cbbasic"
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "📘 Admin Cmd", callback_data="cbadmin"
-                    ),
-                    InlineKeyboardButton(
-                        "📗 Sudo Cmd", callback_data="cbsudo"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "📔 Fun Cmd", callback_data="cbfun"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbstart"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_callback_query(filters.regex("cbhowtouse"))
-async def cbguides(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""🕊️** HOW TO USE THIS BOTT :**
-
-**1.) First, add to your group.
-2.) Then make admin with all permissions except anonymous admin.
-3.) Add @{ASSISTANT_NAME} to your group or type /userbotjoin to invite assistant.
-4.) Turn on voice chat first before playing music.
-
-💡 Bot by @{UPDATES_CHANNEL}**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "BACK", callback_data="cbstart"
                     )
                 ]
             ]

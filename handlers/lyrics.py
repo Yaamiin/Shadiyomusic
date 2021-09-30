b3b5ff9b11c4c809c3f6ea214876cbab93c73cd9
Kennedy <87056837+KennedyProject@users.noreply.github.com>
2021-09-30 23:03:36 +0700

    Update lyrics.py

--- handlers/lyrics.py
+++ handlers/lyrics.py
@@ -1,22 +1,59 @@
-# Module by https://github.com/tofikdn
-# Copyright (C) 2021 TdMusic
+# Modules imported by @xgothboi (https://github.com/KennedyProject/KennedyXMusic)
+# Don't remove this credits
 
-import requests
-from config import BOT_USERNAME
-from pyrogram import Client
+from pyrogram.types import Message
+from python_ARQ import ARQ
+from pyrogram import Client, filters
 from helpers.filters import command
+from config import ARQ_API_KEY
+from config import BOT_USERNAME as bn
+
+aiohttpsession = aiohttp.ClientSession()
+arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
 
 
-@Client.on_message(command(["lyric", f"lyric@{BOT_USERNAME}"]))
-async def lirik(_, message):
-    try:
-        if len(message.command) < 2:
-            await message.reply_text("`Give me the title of the song!!!`")
-            return
-        query = message.text.split(None, 1)[1]
-        rep = await message.reply_text("`Searching lyrics...`")
-        resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
-        result = f"{resp['data']}"
-        await rep.edit(result)
-    except Exception:
-        await rep.edit("`Lyrics not found !\n\n• Try searching with a clearer song title`")
+async def lyrics_func(answers, text):
+    song = await arq.lyrics(text)
+    if not song.ok:
+        answers.append(
+            InlineQueryResultArticle(
+                title="Error",
+                description=song.result,
+                input_message_content=InputTextMessageContent(
+                    song.result
+                ),
+            )
+        )
+        return answers
+    lyrics = song.result
+    song = lyrics.splitlines()
+    song_name = song[0]
+    artist = song[1]
+    if len(lyrics) > 4095:
+        lyrics = await hastebin(lyrics)
+        lyrics = f"**LYRICS_TOO_LONG:** [URL]({lyrics})"
+
+    msg = f"{lyrics}"
+
+    answers.append(
+        InlineQueryResultArticle(
+            title=song_name,
+            description=artist,
+            input_message_content=InputTextMessageContent(msg),
+        )
+    )
+    return answers
+
+
+@Client.on_message(command(["lyric", "lyric@{bn}"]))
+async def lyrics_func(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Usage:**\n/lyric [QUERY]")
+    m = await message.reply_text("**Searching**")
+    query = message.text.strip().split(None, 1)[1]
+    song = await arq.lyrics(query)
+    lyrics = song.result
+    if len(lyrics) < 4095:
+        return await m.edit(f"{lyrics}")
+    lyrics = await paste(lyrics)
+    await m.edit(f"**LYRICS_TOO_LONG:** [URL]({lyrics})") 

5c13e3f65c7222c4877b372f537a060ed121e87d
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-03 12:19:48 +0700

    Update tools.py

--- handlers/tools.py
+++ handlers/tools.py
@@ -1,7 +1,13 @@
-import asyncio
 import os
+import asyncio
+import time
+import shlex
+import requests
+import asyncio
 import re
 import aiofiles
+from typing import Callable, Coroutine, Dict, List, Tuple, Union
+from json import JSONDecodeError
 from pykeyboard import InlineKeyboard
 from pyrogram.types import InlineKeyboardButton
 from helpers.pastebin import paste
@@ -9,6 +15,9 @@ from pyrogram import Client, filters
 from config import BOT_USERNAME, aiohttpsession as session
 from helpers.get_file_id import get_file_id
 from helpers.filters import command
+
+
+# ====== SHOWID ======
 
 
 @Client.on_message(command(["id", f"id@{BOT_USERNAME}"]))
@@ -52,6 +61,8 @@ async def showid(client, message):
                 )
         await message.reply_text(_id, quote=True)
 
+
+# ====== PASTE ======
 
 pattern = re.compile(
     r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$"
@@ -105,3 +116,95 @@ async def paste_func(_, message):
         await m.edit(link)
     else:
         await message.reply_text("Reply To A Message With /paste")
+
+
+# ====== SHAZAM ======
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+async def fetch_audio(client, message):
+    time.time()
+    if not message.reply_to_message:
+        await message.reply("`Reply To A Video / Audio.`")
+        return
+    warner_stark = message.reply_to_message
+    if warner_stark.audio is None and warner_stark.video is None:
+        await message.reply("`Format Not Supported`")
+        return
+    if warner_stark.video:
+        lel = await message.reply("`Video Detected, Converting To Audio !`")
+        warner_bros = await message.reply_to_message.download()
+        stark_cmd = f"ffmpeg -i {warner_bros} -map 0:a friday.mp3"
+        await runcmd(stark_cmd)
+        final_warner = "friday.mp3"
+    elif warner_stark.audio:
+        lel = await edit_or_reply(message, "`Download Started !`")
+        final_warner = await message.reply_to_message.download()
+    await lel.edit("`Almost Done!`")
+    await lel.delete()
+    return final_warner
+
+
+async def edit_or_reply(message, text, parse_mode="md"):
+    if message.from_user.id:
+        if message.reply_to_message:
+            kk = message.reply_to_message.message_id
+            return await message.reply_text(
+                text, reply_to_message_id=kk, parse_mode=parse_mode
+            )
+        return await message.reply_text(text, parse_mode=parse_mode)
+    return await message.edit(text, parse_mode=parse_mode)
+
+
+@Client.on_message(command(["shazam", f"shazam@{BOT_USERNAME}"]))
+async def shazamm(client, message):
+    kek = await edit_or_reply(message, "`Shazaming In Progress!`")
+    if not message.reply_to_message:
+        await kek.edit("Reply To The Audio.")
+        return
+    if os.path.exists("friday.mp3"):
+        os.remove("friday.mp3")
+    kkk = await fetch_audio(client, message)
+    downloaded_file_name = kkk
+    f = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
+    await kek.edit("**Searching For This Song In DataBase.**")
+    r = requests.post("https://starkapi.herokuapp.com/shazam/", files=f)
+    try:
+        xo = r.json()
+    except JSONDecodeError:
+        await kek.edit("`Seems Like Our Server Has Some Issues, Please Try Again Later!`")
+        return
+    if xo.get("success") is False:
+        await kek.edit("`Song Not Found IN Database. Please Try Again.`")
+        os.remove(downloaded_file_name)
+        return
+    xoo = xo.get("response")
+    zz = xoo[1]
+    zzz = zz.get("track")
+    zzz.get("sections")[3]
+    nt = zzz.get("images")
+    image = nt.get("coverarthq")
+    by = zzz.get("subtitle")
+    title = zzz.get("title")
+    messageo = f"""<b>Song Shazamed.</b>
+Song Name : {title}
+Song By : {by}
+Identified Song
+Powered by @{BOT_USERNAME}
+"""
+    await client.send_photo(message.chat.id, image, messageo, parse_mode="HTML")
+    os.remove(downloaded_file_name)
+    await kek.delete()

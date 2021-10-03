a1bf79aa2acc8e02bb60163647189d11964a9b37
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-03 12:22:49 +0700

    Update tools.py

--- handlers/tools.py
+++ handlers/tools.py
@@ -207,3 +207,27 @@ Powered by @{BOT_USERNAME}
     await client.send_photo(message.chat.id, image, messageo, parse_mode="HTML")
     os.remove(downloaded_file_name)
     await kek.delete()
+
+
+@Client.on_message(command(["json", f"json@{BOT_USERNAME}"]))
+async def jsonify(_, message):
+    the_real_message = None
+    reply_to_id = None
+
+    if message.reply_to_message:
+        the_real_message = message.reply_to_message
+    else:
+        the_real_message = message
+
+    try:
+        await message.reply_text(f"<code>{the_real_message}</code>")
+    except Exception as e:
+        with open("json.text", "w+", encoding="utf8") as out_file:
+            out_file.write(str(the_real_message))
+        await message.reply_document(
+            document="json.text",
+            caption=str(e),
+            disable_notification=True,
+            reply_to_message_id=reply_to_id
+        )
+        os.remove("json.text")

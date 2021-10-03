cc53752399b9e90f45ce13812f89de3630d6a227
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-03 10:06:19 +0700

    Update ownerstuff.py

--- handlers/ownerstuff.py
+++ handlers/ownerstuff.py
@@ -202,6 +202,26 @@ async def delvar(client: Client, message: Message, app_):
 # Usage Heroku Dyno
 
 
+def fetch_heroku_git_url(api_key, app_name):
+    if not api_key:
+        return None
+    if not app_name:
+        return None
+    heroku = heroku3.from_key(api_key)
+    try:
+        heroku_applications = heroku.apps()
+    except:
+        return None
+    heroku_app = None
+    for app in heroku_applications:
+        if app.name == app_name:
+            heroku_app = app
+            break
+    if not heroku_app:
+        return None
+    return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
+
+
 @Client.on_message(command("usage"))
 @sudo_users_only
 @_check_heroku

46bd5f96ea5dd9dc9ec8b0991a2bf374a7a18050
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-12 21:31:12 +0700

    Update tools.py

--- handlers/tools.py
+++ handlers/tools.py
@@ -55,10 +55,7 @@ async def telegraph(client, message):
     except Exception as document:
         await message.reply(message, text=document)
     else:
-        await message.reply(
-            f"**Yooo!\n\n👉 https://telegra.ph{response[0]}**",
-            disable_web_page_preview=True,
-        )
+        await message.reply(f"**[https://telegra.ph/file/8bbb813c8f692f434ac3a.mp4](https://telegra.ph{response[0]})**", disable_web_page_preview=False)
     finally:
         os.remove(download_location)
 
